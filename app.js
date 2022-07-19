0dbd718de6be7744d2cf8dbae932e73882e0642f
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2022-07-19 21:07:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,38 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
     ]
 
 var inputs = [];
+var pushingValues;
 
 
-for(i = 0; i <=letters.length; i++){
-    var randomLetter = Math.floor(Math.random()* 26);
-    document.getElementById("letter").innerText += letters[randomLetter];
+// for(i = 0; i <=letters.length; i++){
+//     var randomLetter = Math.floor(Math.random()* 26);
+//     document.getElementById("letter").innerText += letters[randomLetter];
     
 
+// }
+
+function letter(val){
+    console.log(val)
+    document.getElementById("result").value += val;
+    inputs.push(val);
+    pushingValues = inputs.join('')
+    console.log("inputs", pushingValues)
+}
+
+function check(){
+    for(var i = 0; i<=dictionary.length; i++){
+        if(pushingValues == dictionary[i] ){
+            document.getElementById("result").value = "";
+            inputs = []
+            alert("The Word is Correct: "+pushingValues);
+            // break;
+        }
+        // else{
+        // alert("The Word is incorrect");
+        // document.getElementById("result").value = ""
+        // break;
+    // }
+        
+    }
+    
 }
