b458b83af71cce78186e8468d2889165af4945d0
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2022-07-21 16:30:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 var dictionary = ["apple", "ball", "cat", "dog", "pakistan", "banana", "fahad"];
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-    ]
-
 var inputs = [];
 var pushingValues;
 
-
-// for(i = 0; i <=letters.length; i++){
-//     var randomLetter = Math.floor(Math.random()* 26);
-//     document.getElementById("letter").innerText += letters[randomLetter];
-    
-
-// }
 
 function letter(val){
     console.log(val)
@@ -22,21 +11,49 @@ function letter(val){
     pushingValues = inputs.join('')
     console.log("inputs", pushingValues)
 }
-
+var score = 0;
 function check(){
     for(var i = 0; i<=dictionary.length; i++){
         if(pushingValues == dictionary[i] ){
             document.getElementById("result").value = "";
-            inputs = []
+            score++
+            inputs = [];
+            countdown();
             alert("The Word is Correct: "+pushingValues);
             // break;
         }
-        // else{
-        // alert("The Word is incorrect");
-        // document.getElementById("result").value = ""
-        // break;
+    //     else if(pushingValues !== dictionary[i] ){
+    //     alert("The Word is incorrect");
+    //     document.getElementById("result").value = "";
+    //     console.log("incorrect ",pushingValues)
+    //     break;
     // }
         
     }
     
 }
+
+// CountDown
+
+function countdown() {
+    var seconds = 10;
+    function tick() {
+      var counter = document.getElementById("counter");
+      seconds--;
+      counter.innerHTML =
+        "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+      if (seconds > 0) {
+        setTimeout(tick, 1000);
+      } else if(seconds <= 0) {
+        
+        document.getElementById("winner").innerHTML = "<h1> Your Score is " + score + "</h1>"
+      }
+    //   if(counter == 0){
+        
+    //   }
+    }
+    tick();
+  }
+  countdown();
+
+
